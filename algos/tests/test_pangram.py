4b64f0248998ae2cde18ccd04dea478952ae4c91
Felipe Besson <flpbesson@gmail.com>
2014-11-16 13:26:14 -0200

    adds test to pangram

---
+++ algos/tests/test_pangram.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from algos.pangram import is_valid
+
+class TestPangram:
+
+	def test_should_detect_pangram(self):
+		sentence = "We promptly judged antique ivory buckles for the next prize"
+		self.assertTrue(is_valid(sentence))
+
+	def test_should_not_detect_pangram(self):
+		sentence = "We promptly judged antique ivory buckles for the prize"
+		self.assertFalse(is_valid(sentence))	
+
+	def test_should_detect_pangram_with_numbers(self):
+		sentence = "We promptly judged antique ivory buckles for the prize 4 all 2"
+		self.assertFalse(is_valid(sentence))		
