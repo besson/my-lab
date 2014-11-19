c7b6389014aa54f0f0f4bb06041b75c091728b54
Felipe Besson <flpbesson@gmail.com>
2014-11-19 20:30:49 -0200

    test for rotated array

--- algos/tests/test_binary_search_lab.py
+++ algos/tests/test_binary_search_lab.py
@@ -7,3 +7,7 @@ class TestBinarySearchLab(TestCase):
     def test_simple_operation_on_simple_algo(self):
         a = [1, 2, 3, 4, 5, 6, 7, 8]
         self.assertTrue(BinarySearchLab().find(a, 5))
+
+    def test_simple_operation_on_rotated_array(self):
+        a = [4, 5, 6, 7, 8, 1, 2, 3]
+        self.assertTrue(BinarySearchLab().find_enhanced(a, 5))
