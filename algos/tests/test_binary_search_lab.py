dac535f5c4ceeba86668e891bce4b942bdae2abb
Felipe Besson <flpbesson@gmail.com>
2014-11-20 10:23:26 -0200

    more test to binary search lab

--- algos/tests/test_binary_search_lab.py
+++ algos/tests/test_binary_search_lab.py
@@ -8,6 +8,22 @@ class TestBinarySearchLab(TestCase):
         a = [1, 2, 3, 4, 5, 6, 7, 8]
         self.assertTrue(BinarySearchLab().find(a, 5))
 
-    def test_simple_operation_on_rotated_array(self):
+    def test_simple_operation_simple_algo_not_find(self):
+        a = [1, 2, 3, 4, 5, 6, 7, 8]
+        self.assertTrue(BinarySearchLab().find(a, 42))
+
+    def test_simple_operation_on_rotated_array_find_one(self):
+        a = [4, 5, 6, 7, 8, 1, 2, 3]
+        self.assertTrue(BinarySearchLab().find_enhanced(a, 1))
+
+    def test_simple_operation_on_rotated_array_find_5(self):
         a = [4, 5, 6, 7, 8, 1, 2, 3]
         self.assertTrue(BinarySearchLab().find_enhanced(a, 5))
+
+    def test_simple_operation_rotate_array_not_find(self):
+        a = [4, 5, 6, 7, 8, 1, 2, 3]
+        self.assertTrue(BinarySearchLab().find_enhanced(a, 42))
+
+
+
+
