71a1c5bd84bc9f169aa8707145db722bd0232cee
Felipe Besson <flpbesson@gmail.com>
2016-03-19 14:51:38 -0300

    closest solution

--- algos/algos/reverse_shuffle_merge.rb
+++ algos/algos/reverse_shuffle_merge.rb
@@ -13,9 +13,7 @@ class ReverseShuffleMerge
 
   def find input
     @first_letter = input.chars.first.ord
-    @last_letter = input.chars.last.ord
 
-    byebug
     input.chars.each do |w|
       @total_count[w.ord] = 0 unless @total_count[w.ord]
       @total_count[w.ord] += 1
@@ -23,18 +21,16 @@ class ReverseShuffleMerge
 
     @total_count.each do |k,v|
       @first_letter = k if k < @first_letter
-      @last_letter = k if k > @last_letter
 
       @reversed_count[k] = v/2
       @shuffled_count[k] = v/2
     end
 
-    input.chars.each do |w|
+    input.reverse.chars.each do |w|
       if belongs_to_reversed?(w)
-        @reversed_word = w + @reversed_word
+        @reversed_word = @reversed_word + w
         @reversed_count[w.ord] -= 1
       else
-        @shuffled_word = @shuffled_word + w
         @shuffled_count[w.ord] -= 1
       end
 
@@ -48,21 +44,17 @@ class ReverseShuffleMerge
     return false if @reversed_count[w.ord] == 0
     return true if @shuffled_count[w.ord] == 0
 
-    bidx = @first_letter
-    aidx = @last_letter
-    before_sum = 0
-    after_sum = 0
+    idx = @first_letter
+    sum = 0
 
-    while(bidx <= w.ord) do
-      before_sum += @total_count[bidx] if @total_count.key?(bidx)
-      bidx += 1
+    while(idx < w.ord) do
+      sum += @reversed_count[idx] if @reversed_count.key?(idx)
+      idx += 1
     end
 
-    while(aidx > w.ord) do
-      after_sum += @total_count[aidx] if @total_count.key?(aidx)
-      aidx -= 1
-    end
-
-    before_sum > after_sum
+    return sum == 0
   end
 end
+
+input = $stdin.readlines.join
+puts ReverseShuffleMerge.new.find(input)
